79e77f7bb327a40057189ee1a1550762e38fde4e
walaa <walaafaqeh@gmail.com>
2023-05-10 22:07:25 +0300

    new updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://740ce302-a469-4648-a6fe-f3b414c47f20.mock.pstmn.io/api/v1/'
+  apiBaseUrl: 'https://740ce302-a469-4648-a6fe-f3b414c47f20.mock.pstmn.io/csvs'
 };
